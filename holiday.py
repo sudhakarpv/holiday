64b3c8c16ac0edb0b6764af1de3f9356ef5dbe38
sudhakarpv <40256682+sudhakarpv@users.noreply.github.com>
2018-06-25 10:08:30 +0530

    Update holiday.py

--- holiday.py
+++ holiday.py
@@ -2,8 +2,8 @@
 user=input()
 if (user=='Monday'or user=='monday' or user=='Tuesday'or user=='tuesday'or user=='Wednesday'or user=='wednesday'or
 user=='Thursday'or user=='thursday'or user=='friday' or user=='Friday'):
-    print ("No")
+    print ("no")
 elif (user=="Sunday"or user=='sunday'or user=='Saturday'or user=='saturday'):
-    print ("Yes")
+    print ("yes")
 else:
     print("Invalid")
