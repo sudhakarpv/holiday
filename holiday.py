519cc3a61ab6f5441da8c496f292c5dd83793f09
sudhakarpv <40256682+sudhakarpv@users.noreply.github.com>
2018-06-25 10:06:51 +0530

    Update and rename README.md to holiday.py

---
+++ holiday.py
@@ -0,0 +1,9 @@
+# holiday
+user=input()
+if (user=='Monday'or user=='monday' or user=='Tuesday'or user=='tuesday'or user=='Wednesday'or user=='wednesday'or
+user=='Thursday'or user=='thursday'or user=='friday' or user=='Friday'):
+    print ("No")
+elif (user=="Sunday"or user=='sunday'or user=='Saturday'or user=='saturday'):
+    print ("Yes")
+else:
+    print("Invalid")
